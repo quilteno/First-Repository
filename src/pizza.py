702c4837a54e86b23ee985839e9910ec85e8e4d2
quilteno <1416256437@qq.com>
2020-12-26 20:00:36 +0800

    first commit

---
+++ src/pizza.py
@@ -0,0 +1,8 @@
+def make_pizza(size,*toppings):
+    """make which pizza"""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"-{topping}")
+    
+
+    
