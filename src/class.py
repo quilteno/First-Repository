5c2ae1fbfd116ef828fed319445e1f738a16771a
quilteno <1416256437@qq.com>
2020-12-26 21:28:17 +0800

    添加了几个文件

---
+++ src/class.py
@@ -0,0 +1,22 @@
+class Dog:
+    native_pace='wangwang' #类属性
+
+    """all dogs are here"""
+    def _init_(self,name,age): #  函数定义在类里称为方法
+        """
+        defalut option
+        """
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """
+        simulate dog sit recive order
+        """
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """
+        simulate dog rolled order
+        """
+        print(f"{self.name} rolled over!")
