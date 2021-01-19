15ddbc7436be71d7511ee32275a80eb37f1d7bd6
quilteno <1416256437@qq.com>
2021-01-19 19:51:01 +0800

    单词表做了2.0版本,应该是最终版

--- src/test.py
+++ src/test.py
@@ -1,2 +1,16 @@
-a='01234556789'
-print(a[0:10])
+def main():
+    string = 'hellor world!'
+    pl = place('or',string)
+    print(pl)
+
+def place(zi,mu):
+    len1 = len(zi)
+    print(len1)
+    pl = 8
+    for i in range(len(mu)):
+        if mu[i:i+len1] == zi:
+            pl = i
+            break
+    return pl   
+
+main()
--- src/word.py
+++ src/word.py
@@ -1,26 +1,33 @@
-#输入文件
-filename = 'word.txt'
-#输出文件
-output = 'word2.txt'
-#一行中翻译开始的位置
-WIDTH = 20
-
+filename = 'word.txt'  #输入文件
+output = 'word2.txt'   #输出文件
+WIDTH = 20             #一行中翻译开始的位置
+key = ['n.','a.','v.','pron.','prep.','art.','ad.','aux.','vt.','num.','conj.','vi.']
 
 def main():
-    with open(filename, 'r', encoding='UTF-8') as file:#读入文件,按行操作
+    with open(filename, 'r', encoding='UTF-8') as file: #读入文件,按行操作
         for line in file:
             len1 = len(line)
             with open(output,'a') as out:
-                try:                        #用try可避免个别单词无空格
-                    pl = line.index(' ')
-                    a = [line[0:pl+1]]
-                    b = [' ']
-                    c = [line[pl:len1+1]]
-                    a = a + b*(WIDTH-pl) + c
-                    for each in a:
-                        out.write(each)
-                except ValueError:          #无空格的单词直接输出
-                    out.write(line)
-      #如果无空格,则输入至窗口
+                pl = 20
+                for i in range(len(key)):
+                    pls = place(key[i],line)
+                    if pls < pl:
+                        pl = pls
+                #print(pl,end='')             
+                a = [line[0:pl]]
+                b = [' ']
+                c = [line[pl:len1+1]]
+                a = a + b*(WIDTH-pl) + c      #拼接字符串,在中间加上空格 
+                for each in a:
+                    out.write(each)
+
+def place(zi,mu):
+    len1 = len(zi)
+    pl = 20
+    for i in range(len(mu)):
+        if mu[i:i+len1] == zi:
+            pl = i
+            break
+    return pl   
 
 main()
