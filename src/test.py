5567df3d2fd0291a755d79e1268d647f1ead39ad
quilteno <1416256437@qq.com>
2021-01-21 00:20:54 +0800

    没干啥

--- src/test.py
+++ src/test.py
@@ -1,22 +1,17 @@
-filein = 'test.txt'
+filein = 'word1111.txt'
 fileout = 'testout.txt'
 
 with open(filein, 'r',encoding='UTF-8') as file:
     count = 0
-    
-    string1 = string2 = ''
+    pl = 0
     for line in file:
-        #print(f"{'*'}{int((count / 10)) % 2}")
-        if int((count / 10)) % 2 == 0:
-            string1 += line
-        elif int((count / 10)) % 2 == 1:
-            string2 = line + string2
-            #print(string2)
-        if (count) % 320 == 0 and count != 0:
-            with open(fileout,'a') as out:
-                print(string1[:20] + string2[:20])
-                out.write(string1 + string2)
-            string1 = string2 = ''
+        try:
+            pls = line.index(' ')
+            if pls > pl:
+                pl = pls
+                a = count
+        except ValueError:
+            pass    
         count += 1
-    with open(fileout,'a') as out:
-            out.write(string1 + string2)
+    print(a)
+    print(pl)
