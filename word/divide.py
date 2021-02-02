eaa85b738b76d36e46ee0c87e8d54390025d4d43
quilteno <1416256437@qq.com>
2021-02-02 19:10:11 +0800

    divide分为等距三列

--- word/divide.py
+++ word/divide.py
@@ -2,22 +2,15 @@ import wordf
 
 filename = 'word/word.txt'
 outfile1 = 'word/part1.txt'
-outfile2 = 'word/part2.txt'
-outfile3 = 'word/part3.txt'
 
-wordf.delete([outfile1,outfile2,outfile3])
+wordf.delete([outfile1])
 
 with open(filename,'r',encoding='UTF-8') as file:
     for line in file:
         if line[0] >= 'A':
-            a = line[:26]
-            b = line[26:-4]
-            c = line[-4:]
-            with open(outfile1,'a',encoding='UTF-8') as out1,\
-                open(outfile2,'a',encoding='UTF-8') as out2,\
-                    open(outfile3,'a',encoding='UTF-8') as out3:
-                out1.write(a.replace(' ',''))
-                out1.write('\r')
-                out2.write(b.replace(' ',''))
-                out2.write('\r')
-                out3.write(c.replace(' ',''))
+            a = line[:20].replace(' ','')
+            b = line[22:-4].replace(' ','')
+            c = line[-4:].replace(' ','')
+            a = a + '\t' + b + '\t' + c
+            with open(outfile1,'a',encoding='UTF-8') as out1:
+                out1.write(a)
